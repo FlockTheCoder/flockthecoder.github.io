fa0e0fc44b44f74a8e559d0f4a4fab439c98d7e3
FlockTheCoder <flockthecoder@chello.at>
2022-11-01 01:54:11 +0100

    adding a new subsite -> color basics

---
+++ subsites/colorthings/color.js
@@ -0,0 +1,12 @@
+
+var usrColor = "";
+
+var usrColorInputButton = Document.querySelector(".hex-color-btn");
+var usrColorInput = Document.querySelector(".hex-color-input")
+
+usrColorInputButton.addEventListener("click", function() {
+
+
+
+    
+})
